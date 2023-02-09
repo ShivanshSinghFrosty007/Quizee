cfaf1c77402659a9cffef39c6db6531199c51d6c
Shivanshsinghfrosty <119441847+ShivanshSinghFrosty007@users.noreply.github.com>
2023-02-09 17:42:22 +0530

    commit

--- app/src/main/java/com/example/quizee/MainActivity.java
+++ app/src/main/java/com/example/quizee/MainActivity.java
@@ -4,16 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 2000;
+
+    TextView logo;
+    Animation topAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(getApplicationContext(),QuizChooser.class);
-        startActivity(intent);
-        finish();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+
+        logo = findViewById(R.id.logo);
+        logo.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),QuizChooser.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
